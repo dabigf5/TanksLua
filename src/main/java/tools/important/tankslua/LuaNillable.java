13f6c64d7c182e34e12cae26669421749a2cb641
F6 <dabsi@dabserver.com>
2024-02-06 18:59:43 -0600

    update LuaNillable

--- src/main/java/tools/important/tankslua/LuaNillable.java
+++ src/main/java/tools/important/tankslua/LuaNillable.java
@@ -3,6 +3,8 @@ package tools.important.tankslua;
 
 import javax.annotation.processing.SupportedAnnotationTypes;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
@@ -10,5 +12,6 @@ import java.lang.annotation.Target;
  */
 @Target(ElementType.FIELD)
 @SupportedAnnotationTypes({"LuaValue"})
+@Retention(RetentionPolicy.SOURCE)
 public @interface LuaNillable {
 }
