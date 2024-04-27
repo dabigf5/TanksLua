8f43a8348d7726b64e686da2a207d98e5fc314f0
F6 <dabsi@dabserver.com>
2024-04-27 15:00:44 -0500

    need space

--- src/main/java/tools/important/tankslua/luapackage/packsource/ZipFilePackSource.java
+++ src/main/java/tools/important/tankslua/luapackage/packsource/ZipFilePackSource.java
@@ -2,7 +2,7 @@ package tools.important.tankslua.luapackage.packsource;
 
 import java.io.File;
 
-public class ZipFilePackSource implements PackSource{
+public class ZipFilePackSource implements PackSource {
     //todo: this class
     @Override
     public File getFile(String fileName) {
