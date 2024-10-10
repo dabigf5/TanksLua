236c0250c27b001ac80e221fdc19334663209b81
F6 <dabsi@dabserver.com>
2024-10-10 09:26:03 -0500

    move tanks lib and add drawing component

--- src/tools/important/tankslua/LuaStateInitialize.kt
+++ src/tools/important/tankslua/LuaStateInitialize.kt
@@ -1,12 +1,7 @@
 package tools.important.tankslua
 
-import party.iroiro.luajava.JFunction
 import party.iroiro.luajava.Lua
 import party.iroiro.luajava.LuaException
-import tanks.Game
-import tanks.ModAPI
-import tools.important.tankslua.gui.Notification
-import tools.important.tankslua.gui.NotificationType
 
 fun luaError(message: String): Nothing = throw LuaException(LuaException.LuaError.RUNTIME, message)
 
@@ -25,37 +20,5 @@ fun Lua.initialize() {
 
     run("io.stdout:setvbuf'no'")
 
-    loadTanksLib(this)
-}
-
-fun loadTanksLib(luaState: Lua) {
-    luaState.createTable(0, 0)
-    val tanksLib = luaState.get()
-
-    tanksLib.set("notify", JFunction(fun(state): Int {
-        val argCount = state.top
-
-        val message: String = if (argCount >= 1) {
-            state.toString(1) ?: luaError("Message is wrong type!")
-        } else luaError("Not enough arguments!")
-
-        val duration: Double? = if (argCount >= 2) {
-            if (!state.isNumber(2)) luaError("Duration is wrong type!")
-            state.toNumber(2)
-        } else null
-
-        Notification(message, NotificationType.INFO, duration ?: 200.0)
-        return 0
-    }))
-
-    tanksLib.set("version", Game.version)
-
-    tanksLib.set("isModApi", try {
-        ModAPI::class.java.getDeclaredField("version")
-        true
-    } catch (_: NoSuchFieldException) {
-        false
-    })
-
-    luaState.set("tanks", tanksLib)
+    openTanksLib(this)
 }
